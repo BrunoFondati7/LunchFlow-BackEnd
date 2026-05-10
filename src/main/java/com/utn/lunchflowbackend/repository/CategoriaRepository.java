3c6995044a3231522a1af72afe661024c28a7846
savant741 <brunofondati7@gmail.com>
2026-05-10 15:42:45 -0300

    fix: implementar entidades de Plato y Categoria para el catalogo del administrador

--- src/main/java/com/utn/lunchflowbackend/repository/CategoriaRepository.java
+++ src/main/java/com/utn/lunchflowbackend/repository/CategoriaRepository.java
@@ -1,4 +1,10 @@
 package com.utn.lunchflowbackend.repository;
 
-public class CategoriaRepository {
+import com.utn.lunchflowbackend.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    // Vacío también está perfecto.
 }
--- src/main/java/com/utn/lunchflowbackend/repository/PlatoRepository.java
+++ src/main/java/com/utn/lunchflowbackend/repository/PlatoRepository.java
@@ -1,4 +1,10 @@
 package com.utn.lunchflowbackend.repository;
 
-public class PlatoRepository {
+import com.utn.lunchflowbackend.model.Plato;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlatoRepository extends JpaRepository<Plato, Long> {
+    // No hace falta escribir nada acá por ahora.
 }
